c11eec4a4ab0b95909f6354ba31932319cfca0ae
Francisco Zambrano <136658451+Francisco-Zambrano@users.noreply.github.com>
2024-04-30 15:55:54 -0300

    fixing messages issues

--- src/app.js
+++ src/app.js
@@ -1,51 +1,60 @@
 import express from 'express';
 import path from 'path';
 import __dirname from './utils.js';
-import {engine} from 'express-handlebars';
+import { engine } from 'express-handlebars';
 import { Server } from "socket.io";
-import {router as viewsRouter} from './routes/views.router.js';
+import { router as viewsRouter } from './routes/views.router.js';
 
-const PORT=3000;
-const app=express();
+const PORT = 3000;
+const app = express();
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname,'/views'));
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.use('/', viewsRouter)
+app.use('/', viewsRouter);
 
-let usuarios=[]
-let mensajes=[]
+let usuarios = [];
+let messages = [];
 
-const server=app.listen(PORT,()=>{
+const server = app.listen(PORT, () => {
+
     console.log(`Server listening on port ${PORT}`);
+
 });
 
-const io = new Server(server)
+const io = new Server(server);
 
-io.on("connection", socket=>{
-    console.log(`A client with id has connected ${socket.id}`)
+io.on("connection", socket => {
 
-    socket.on("id", name=>{
-        usuarios.push({id:socket.id, name})
-        socket.emit("mensajesPrevios", mensajes)
-        socket.broadcast.emit("nuevoUsuario", name)
-    })
+    console.log(`A client with id has connected ${socket.id}`);
 
-    socket.on("mensaje", (name, mensaje)=>{
-        mensajes.push({name, mensaje})
-        io.emit("nuevoMensaje", name, mensaje)
-    })
+    socket.on("id", name => {
 
-    socket.on("disconnect", ()=>{
-        let usuario=usuarios.find(u=>u.id===socket.id)
-        if(usuario){
-            io.emit("saleUsuario", usuario.name)
+        usuarios.push({ id: socket.id, name });
+        socket.emit("previousMessages", messages);
+        socket.broadcast.emit("newUser", name);
+
+    });
+
+    socket.on("message", (name, message) => {
+
+        messages.push({ name, message });
+        io.emit("newMessage", name, message);
+
+    });
+
+    socket.on("disconnect", () => {
+
+        let usuario = usuarios.find(u => u.id === socket.id);
+        if (usuario) {
+            io.emit("userExits", usuario.name);
         }
-    })
-})
+        
+    });
+});
--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1,56 +1,65 @@
 Swal.fire({
-  title:"Write your name",
-  input:"text",
-  inputValidator: (value)=>{
-      return !value && "You must enter a name...!!!"
-  },
-  allowOutsideClick:false
-}).then(data=>{
-  
-  let name=data.value
-  document.title=name
 
-  let inputMensaje=document.getElementById("mensaje")
-  let divMensajes=document.getElementById("mensajes")
-  inputMensaje.focus()
-  
-  const socket=io()
-  
-  socket.emit("id", name)
+    title: "Write your name",
+    input: "text",
+    inputValidator: (value) => {
+        return !value && "You must enter a name...!!!";
+    },
+    allowOutsideClick: false
 
-  socket.on("nuevoUsuario", name=>{
-      Swal.fire({
-          text:`${name} has logged in...!!!`,
-          toast:true,
-          position:"top-right"
-      })
-  })
+}).then(data => {
+    
+    let name = data.value;
+    document.title = name;
 
-  socket.on("mensajesPrevios", mensajes=>{
-      mensajes.forEach(m=>{
-          divMensajes.innerHTML+=`<span class="mensaje"><strong>${m.name}</strong> says: <i>${m.mensaje}</i></span><br>`
-          divMensajes.scrollTop=divMensajes.scrollHeight
-      })
-  })
+    let inputMessage = document.getElementById("message");
+    let divMessages = document.getElementById("messages");
+    inputMessage.focus();
 
-  socket.on("saleUsuario", name=>{
-      divMensajes.innerHTML+=`<span class="mensaje"><strong>${name}</strong> has left the chat... :(</span><br>`
-      divMensajes.scrollTop=divMensajes.scrollHeight
-  })
+    const socket = io();
 
-  inputMensaje.addEventListener("keyup", e=>{
-      e.preventDefault()
+    socket.emit("id", name);
 
-      if(e.code==="Enter" && e.target.value.trim().length>0){
-          socket.emit("mensaje", name, e.target.value.trim())
-          e.target.value=""
-          e.target.focus()
-      }
-  })
+    socket.on("newUser", name => {
 
-  socket.on("nuevoMensaje", (name, mensaje)=>{
-      divMensajes.innerHTML+=`<span class="mensaje"><strong>${name}</strong> says: <i>${mensaje}</i></span><br>`
-      divMensajes.scrollTop=divMensajes.scrollHeight
-  })
+        Swal.fire({
+            text: `${name} has logged in!!!`,
+            toast: true,
+            position: "top-right"
+        });
 
-})
+    });
+
+    socket.on("previousMessages", messages => {
+
+        messages.forEach(m => {
+            divMessages.innerHTML += `<span class="message"><strong>${m.name}</strong> says: <i>${m.message}</i></span><br>`;
+            divMessages.scrollTop = divMessages.scrollHeight;
+        });
+
+    });
+
+    socket.on("userExits", name => {
+
+        divMessages.innerHTML += `<span class="message"><strong>${name}</strong> has left the chat </span><br>`;
+        divMessages.scrollTop = divMessages.scrollHeight;
+
+    });
+
+    inputMessage.addEventListener("keyup", e => {
+        e.preventDefault();
+
+        if (e.code === "Enter" && e.target.value.trim().length > 0) {
+            socket.emit("message", name, e.target.value.trim());
+            e.target.value = "";
+            e.target.focus();
+        }
+    });
+
+    socket.on("newMessage", (name, message) => {
+
+        divMessages.innerHTML += `<span class="message"><strong>${name}</strong> says: <i>${message}</i></span><br>`;
+        divMessages.scrollTop = divMessages.scrollHeight;
+
+    });
+});
